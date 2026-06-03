10b893fb2bd3d32f70f9aeec1756de6856d3d4a1
Luca Pallagi <luca.pallagi@stud.kbw.ch>
2026-06-03 09:14:46 +0200

    Erstelle Hash-Router

--- trainingapp/src/App.jsx
+++ trainingapp/src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
-
+import { HashRouter, Route, Routes, Link } from "react-router-dom";
+import Startseite from "./comp/Startseite.jsx"
 
 function App() {
 
   return (
     <>
+        <HashRouter>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<Startseite />} />
+          </Routes>
+        </HashRouter>
 
     </>
   )
